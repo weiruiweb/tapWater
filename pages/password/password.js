4226ef2cb61027d38c68bedbce19e4b8e7726e0e
weiruiweb <40764254+weiruiweb@users.noreply.github.com>
2018-07-05 19:37:54 +0800

    password

--- pages/password/password.js
+++ pages/password/password.js
@@ -4,51 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+   isShowPassword:true,
+   isShowPassword1:true,
+   isShowPassword2:true
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+  
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  showEye:function(){
+    // var ids = e.currentTarget.dataset.id;
+    var isShowPasswordEye = !this.data.isShowPassword;
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      isShowPassword:isShowPasswordEye
     })
-  }
+
+  },
+  showEye1:function(){
+    // var ids = e.currentTarget.dataset.id;
+    var isShowPasswordEye = !this.data.isShowPassword1;
+    this.setData({
+      isShowPassword1:isShowPasswordEye
+    })
+
+  },
+  showEye2:function(){
+    // var ids = e.currentTarget.dataset.id;
+    var isShowPasswordEye = !this.data.isShowPassword2;
+    this.setData({
+      isShowPassword2:isShowPasswordEye
+    })
+
+  },
+
+
 })
